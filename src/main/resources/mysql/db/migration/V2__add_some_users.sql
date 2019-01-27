cccc5a832f08f62c1a13f6b22cfc38ff84505940
Stephane Eybert <mittiprovence@yahoo.se>
2019-01-27 22:58:40 +0200

    Added an sql script to add some users.

---
+++ src/main/resources/mysql/db/migration/V2__add_some_users.sql
@@ -0,0 +1,8 @@
+INSERT INTO `user_account` VALUES (1,0,null,null,'Stephane','Eybert','bWl0dGlwcm92ZW5jZUB5YWhvby5zZTptaWduZXRudWxs',NULL,NULL,'mittiprovence@yahoo.se',false,NULL);
+INSERT INTO `user_role` VALUES (1,0,null,null,1,'ROLE_USER');
+INSERT INTO `user_account` VALUES (2,0,null,null,'Marc','Eybert','bWl0dGlwcm92ZW5jZUB5YWhvby5zZTptaWduZXRudWxs',NULL,NULL,'marc@yahoo.fr',false,NULL);
+INSERT INTO `user_role` VALUES (2,0,null,null,2,'ROLE_ADMIN');
+INSERT INTO `user_account` VALUES (3,0,null,null,'Cyril','Eybert','bWl0dGlwcm92ZW5jZUB5YWhvby5zZTptaWduZXRudWxs',NULL,NULL,'cyril@yahoo.es',false,NULL);
+INSERT INTO `user_role` VALUES (3,0,null,null,3,'ROLE_ADMIN');
+INSERT INTO `user_account` VALUES (4,0,null,null,'Ondrej','Steffl','b25kcmVqLnN0ZWZmbEBlZHVjYXNvZnQuY3o6aW5wcmFndWVudWxs',NULL,NULL,'ondrej.steffl@educasoft.cz',false,NULL);
+INSERT INTO `user_role` VALUES (4,0,null,null,4,'ROLE_ADMIN');
