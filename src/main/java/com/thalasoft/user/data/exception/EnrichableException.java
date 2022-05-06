67affe19df52317b61053650a1d664af1506be0f
Stephane Eybert <mittiprovence@yahoo.se>
2022-05-06 10:19:47 +0200

    Formatted the source code

--- src/main/java/com/thalasoft/user/data/exception/EnrichableException.java
+++ src/main/java/com/thalasoft/user/data/exception/EnrichableException.java
@@ -5,106 +5,121 @@ import java.util.List;
 
 public abstract class EnrichableException extends RuntimeException {
 
-	public static final long serialVersionUID = -1;
+  protected final List<InfoItem> infoItems = new ArrayList<>();
 
-	protected List<InfoItem> infoItems = new ArrayList<InfoItem>();
+  protected class InfoItem {
+    private String errorMessage = null;
+    private String errorCode = null;
+    private String errorContext = null;
 
-	protected class InfoItem {
-		public String errorMessage = null;
-		public String errorCode = null;
-		public String errorContext = null;
+    public InfoItem(String errorContext, String errorCode, String errorMessage) {
+      this.errorContext = errorContext;
+      this.errorCode = errorCode;
+      this.errorMessage = errorMessage;
+    }
 
-		public InfoItem(String errorMessage, String errorCode, String contextCode) {
-			this.errorMessage = errorMessage;
-			this.errorCode = errorCode;
-			this.errorContext = contextCode;
-		}
+    public InfoItem(String errorContext, String errorCode) {
+      this.errorContext = errorContext;
+      this.errorCode = errorCode;
+    }
 
-		public InfoItem(String errorMessage, String errorCode) {
-			this.errorMessage = errorMessage;
-			this.errorCode = errorCode;
-		}
+    public InfoItem(String errorContext) {
+      this.errorContext = errorContext;
+    }
+  }
 
-		public InfoItem(String errorMessage) {
-			this.errorMessage = errorMessage;
-		}
-	}
+  protected EnrichableException() {
+  }
 
-	public EnrichableException() {
-	}
+  protected EnrichableException(String message) {
+    super(message);
+    this.infoItems.add(new InfoItem(message));
+  }
 
-	public EnrichableException(String errorMessage) {
-		super(errorMessage);
-		this.infoItems.add(new InfoItem(errorMessage));
-	}
+  protected EnrichableException(Throwable cause) {
+    super(cause);
+  }
 
-	public EnrichableException(String errorMessage, String errorCode) {
-		super(errorMessage);
-		this.infoItems.add(new InfoItem(errorMessage, errorCode, errorCode));
-	}
+  protected EnrichableException(String message, Throwable cause) {
+    super(message, cause);
+    this.infoItems.add(new InfoItem(message));
+  }
 
-	public EnrichableException(String errorMessage, String errorCode, String errorContext) {
-		super(errorMessage);
-		this.infoItems.add(new InfoItem(errorMessage, errorCode, errorContext));
-	}
+  protected EnrichableException(String errorMessage, String errorCode) {
+    super(errorMessage);
+    this.infoItems.add(new InfoItem(errorMessage, errorCode, errorCode));
+  }
 
-	public EnrichableException(String errorMessage, String errorCode, String errorContext, Throwable cause) {
-		super(errorMessage, cause);
-		this.infoItems.add(new InfoItem(errorMessage, errorCode, errorContext));
-	}
+  protected EnrichableException(String errorMessage, String errorCode, String errorContext) {
+    super(errorMessage);
+    this.infoItems.add(new InfoItem(errorMessage, errorCode, errorContext));
+  }
 
-	public String getCode() {
-		StringBuilder builder = new StringBuilder();
+  protected EnrichableException(String errorMessage, String errorCode, String errorContext, Throwable cause) {
+    super(errorMessage, cause);
+    this.infoItems.add(new InfoItem(errorMessage, errorCode, errorContext));
+  }
 
-		for (int i = this.infoItems.size() - 1; i >= 0; i--) {
-			InfoItem info = this.infoItems.get(i);
-			builder.append('[');
-			builder.append(info.errorContext);
-			builder.append(':');
-			builder.append(info.errorCode);
-			builder.append(']');
-		}
+  public EnrichableException addContext(String errorMessage, String errorCode, String errorContext) {
+    this.infoItems.add(new InfoItem(errorMessage, errorCode, errorContext));
+    return this;
+  }
 
-		return builder.toString();
-	}
+  public String getContext() {
+    StringBuilder builder = new StringBuilder();
 
-	public String toString() {
-		StringBuilder builder = new StringBuilder();
+    for (int i = this.infoItems.size() - 1; i >= 0; i--) {
+      InfoItem info = this.infoItems.get(i);
+      builder.append('[')
+      .append(info.errorContext)
+      .append(':')
+      .append(info.errorCode)
+      .append(':')
+      .append(info.errorMessage)
+      .append(']');
+    }
 
-		builder.append(getCode());
-		builder.append('\n');
+    return builder.toString();
+  }
 
-		for (int i = this.infoItems.size() - 1; i >= 0; i--) {
-			InfoItem info = this.infoItems.get(i);
-			builder.append('[');
-			builder.append(info.errorContext);
-			builder.append(':');
-			builder.append(info.errorCode);
-			builder.append(']');
-			builder.append(info.errorMessage);
-			if (i > 0)
-				builder.append('\n');
-		}
+  @Override
+  public String toString() {
+    StringBuilder builder = new StringBuilder();
 
-		if (getMessage() != null) {
-			builder.append('\n');
-			if (getCause() == null) {
-				builder.append(getMessage());
-			} else if (!getMessage().equals(getCause().toString())) {
-				builder.append(getMessage());
-			}
-		}
-		appendException(builder, getCause());
+    builder.append(getContext());
+    builder.append('\n');
 
-		return builder.toString();
-	}
+    for (int i = this.infoItems.size() - 1; i >= 0; i--) {
+      InfoItem info = this.infoItems.get(i);
+      builder.append('[')
+      .append(info.errorContext)
+      .append(':')
+      .append(info.errorCode)
+      .append(':')
+      .append(info.errorMessage)
+      .append(']');
+      if (i > 0)
+        builder.append('\n');
+    }
 
-	private void appendException(StringBuilder builder, Throwable throwable) {
-		if (throwable == null)
-			return;
-		appendException(builder, throwable.getCause());
-		builder.append(throwable.toString());
-		builder.append('\n');
-	}
+    if (getMessage() != null) {
+      builder.append('\n');
+      if (getCause() == null || !getMessage().equals(getCause().toString())) {
+        builder.append(getMessage());
+      }
+    }
+    appendException(builder, getCause());
+
+    return builder.toString();
+  }
+
+  private void appendException(StringBuilder builder, Throwable throwable) {
+    if (throwable == null) {
+      return;
+    }
+    appendException(builder, throwable.getCause());
+    builder.append(throwable.toString());
+    builder.append('\n');
+  }
 
 }
